c5eb0564185503cd8a12100334f7f684ec862198
Lukasdotcom <72501726+Lukasdotcom@users.noreply.github.com>
2020-10-29 09:58:26 -0400

    v0.2.1 files
    These are the files neccessary to allow for updating

---
+++ updateChecker.py
@@ -0,0 +1,45 @@
+files = ["battle.py", "CONTRIBUTING.md", "HELP.MD", "LICENSE", "README.md", "shop.py", "start.py", "updater.py", "autosave.json"] #List of all files that need to be deleted except for this file
+update = input("Enter y if you would like to check for an update otherwise enter anything else: ") #Asks if the user wants to check for an update
+try:#Removes the old version.json if it exists
+    os.remove("version.json")
+except:
+    None
+if update == "y":
+    #Downloads the json file from github and reads it
+    wget.download("https://github.com/Lukasdotcom/RRLG/blob/bugfix/version.json")
+    file = open("version.json",)
+    update = json.load(file)
+    file.close
+    if update["version"] != "v0.2": #Checks that a new version was released
+        #Notifies the user that there is a new version and what it is and tells the user if this program crashes during the update process to reinstall the program
+        print(update["version"] + " is out. It will be installed now.")
+        print("If this program crashes please reinsall the program from github.")
+        if update["type"] == "1": #Checks that the update uses the same protocol as the one used here
+            print("cleaning...")
+            try:#Saves the information in the save file
+                file = open("autoSave.json",)
+                saveFile = json.load(file)
+                file.close
+            except: #If there is no save file it says False instead
+               saveFile = False 
+            for x in files: #deletes all the old program files
+                try:
+                    os.remove(x)
+                except:
+                    None
+            save = [2, saveFile] #First one is the save version used and the second one is the information
+            print("Downloading...")
+            wget.download(update["link"]) #Downloads the new program files
+            print("Extracting...")
+            #Extracts the downloaded zip file
+            zip = zipfile.ZipFile(update["zip"]) 
+            zip.extractall()
+            zip.close()
+            os.remove(update["zip"]) #deletes the zip file
+            currentDirectory = os.getcwd() #Finds the currentDirectory
+            os.rename((currentDirectory + "/" + update["folder"] + "/updater.py"), (currentDirectory + "/updater.py")) #Moves updater.py out of the extracted folder
+            exec(open("updater.py").read())
+        else:#Notifies user that the update protocol has changed
+            print("This program does not support auto update anymore please open https://github.com/Lukasdotcom/RRLG/releases to get the latest version.")
+    else: #Notifies user that they are on the latest version
+        print("You are on the latest version")
---
+++ updater.py
@@ -0,0 +1,18 @@
+files = ["battle.py", "CONTRIBUTING.md", "HELP.MD", "LICENSE", "README.md", "shop.py", "start.py", "updateChecker.py", "autosave.json"] #list of all the files that exist in this program
+os.remove("updateChecker.py")#Removes the old update checker
+print("Conerting saves...")
+#This will convert the save
+if save[1] != False #looks if a save file even exists
+    try:
+        #will create the save file if possible
+        player = save[1]
+        with open("autoSave.json", "w") as file: 
+            json.dump(player, file)
+    except:#If save file creation fails will tell user
+        print("Your save file was corrupted during the update process")
+print("Preparing game...")
+for x in files #moves all the files out into the correct location
+    os.rename((currentDirectory + "/" + update["folder"] + "/" + x), (currentDirectory + "/" + x))
+os.remove(update["folder"]) #removes the old folder
+print("Finished starting program.")
+exec(open("start.py").read())#starts the game
