37fe91f3bcd754f8785077b87f266b36772bf91d
Lukasdotcom <72501726+Lukasdotcom@users.noreply.github.com>
2020-10-29 10:31:59 -0400

    Fixes syntax errors

--- updater.py
+++ updater.py
@@ -2,7 +2,7 @@ files = ["battle.py", "CONTRIBUTING.md", "HELP.MD", "LICENSE", "README.md", "sho
 os.remove("updateChecker.py")#Removes the old update checker
 print("Conerting saves...")
 #This will convert the save
-if save[1] != False #looks if a save file even exists
+if save[1] != False: #looks if a save file even exists
     try:
         #will create the save file if possible
         player = save[1]
@@ -11,7 +11,7 @@ if save[1] != False #looks if a save file even exists
     except:#If save file creation fails will tell user
         print("Your save file was corrupted during the update process")
 print("Preparing game...")
-for x in files #moves all the files out into the correct location
+for x in files: #moves all the files out into the correct location
     os.rename((currentDirectory + "/" + update["folder"] + "/" + x), (currentDirectory + "/" + x))
 os.remove(update["folder"]) #removes the old folder
 print("Finished starting program.")
